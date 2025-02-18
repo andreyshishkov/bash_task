8588fa6820323440910fd51061e935c699eef37f
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-02-18 17:48:42 +0300

    Add logic to find the most popular url

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -15,3 +15,16 @@ echo -e "" >> report.txt
 
 echo "Количество запросов по методам:" >> report.txt
 awk '{sub(/"/, "", $6); counts[$6]++} END {for (val in counts) print "\t", counts[val], val}' $logs_file >> report.txt
+
+echo "" >> report.txt
+
+most_popular_url="$(awk '{counts[$7]++} END {
+    max_count=0; max_value=""; 
+    for (val in counts) {
+        if (counts[val] > max_count) {max_count = counts[val]; max_value = val}
+        }
+    print max_count, "\t", max_value
+    }' $logs_file
+)"
+
+echo -e "Самый популярный URL:\t${most_popular_url}" >> report.txt
