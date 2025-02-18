713071f5fdd16fbd086de17320dfa46f006ae421
Andrey Shishkov <shishkoffandrey@yandex.ru>
2025-02-18 03:05:08 +0300

    Add unique IP-addresses counting

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -7,3 +7,6 @@ echo "========================" >> report.txt
 
 total_requests="$(awk 'END {print NR}' $logs_file)"
 echo -e "Общее количество запросов:\t${total_requests}" >> report.txt
+
+unique_ip_count="$(awk '!U[$1]++{count++} END {print count}' $logs_file)"
+echo -e "Количество уникальных IP-адресов:\t${unique_ip_count}" >> report.txt
